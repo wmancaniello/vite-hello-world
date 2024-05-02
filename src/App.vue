1c19ae84c4139d3cb545a60e032b7974915276a7
wmancaniello <wmancaniello@gmail.com>
2024-05-02 10:31:51 +0200

    Completed

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppBanner from "./components/AppBanner.vue";
+
+export default {
+  components: {
+    AppBanner,
+  },
+  data() {
+    return {
+      title: "La mia prima app con VITE!",
+    };
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ title }}</h1>
+
+  <section>
+    <AppBanner />
+  </section>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="banner">test Banner</div>
+</template>
+
+<style>
+.banner {
+  background-color: lightcoral;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+}
+</style>
